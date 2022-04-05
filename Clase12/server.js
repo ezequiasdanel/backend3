26716a1f057c20b754001839eb6c9d594214379c
ezequiasdanel <ezequiasdanel@hotmail.com>
2022-04-05 14:38:06 -0300

    commit5

--- Clase12/server.js
+++ Clase12/server.js
@@ -1,3 +1,4 @@
+const path = require('path');
 const express = require('express');
 const { Server: HttpServer } = require('http')
 const { Server: IOServer } = require('socket.io')
@@ -6,39 +7,30 @@ const app = express()
 const httpServer = new HttpServer(app)
 const io = new IOServer(httpServer)
 
-// app.use(express.static('/public'))
-// app.set('public','./public')
 app.set('view engine', 'ejs')
+app.set('views', __dirname + '/views')
+
+httpServer.listen(3000, ()=> console.log('server on'))
 
 const messages = [
-    {email:'pepito@hotmail.com', text: 'Hola mundo!'}   
+    {email:'pepito@hotmail.com', texto: 'Hola mundo!'}   
 ];
 
 const datos = [];
 
 app.get('/', (req, res)=>{
-    res.render('public/index', {datos, messages})
+    res.render('index', {datos, messages})
 })
-// app.post('/', (req, res)=>{
-//     datos.push(req.body)
-//     res.render('public/index',{datos, messages})
-// })
 
-httpServer.listen(3000, ()=> console.log('server on'))
-
-// io.on('connection', function(socket){
-//     console.log('un cliente se ha conectado')
-//     socket.emit('messages',messages)
-//     socket.on('new-message', data =>{
-//         messages.push(data);
-//         io.sockets.emit('messages',messages)
-//     })
-// })
 io.on('connection', function(socket) { 
     console.log('Un cliente se ha conectado');
-     socket.on('new-dato', (data) => { datos.push(data); io.sockets.emit('datos', {datos}); 
+     socket.on('new-dato', (data) => { 
+         datos.push(data); io.sockets.emit('datos', {datos}); 
     });
 
-    socket.on('new-message', (data) => { messages.push(data); io.sockets.emit('messages', {messages}); });
+    socket.on('new-message', (data) => {
+         messages.push(data); io.sockets.emit('messages', {messages}); });
 
 });
+
+app.use(express.static(path.join(__dirname, 'public')))
